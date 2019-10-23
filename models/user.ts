401a840d70c829165675a84aa01b0e220ab419e0
Renat <rkapenov@ronasit.com>
2019-10-23 15:42:15 +0600

    refactoring user model

--- models/user.ts
+++ models/user.ts
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 const userSchema = new mongoose.Schema({
     email: {type: String, unique: true, lowercase: true},
     name: {type: String},
-    hash: {type: String, select: false},
+    hash: {type: String},
     isActivate: {type: Boolean, default: false}
 });
 
@@ -21,7 +21,7 @@ userSchema.methods.checkPassword = function(password: string) {
 userSchema.methods.createToken = async function() {
     return await jwt.sign({
         data: {_id: this._id, name: this.name}
-    }, 'manager', { expiresIn: 60 * 60 });
+    }, 'manager', { expiresIn: 10 });
 };
 
 const User = mongoose.model('User', userSchema);
